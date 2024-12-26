7a4e9b20c7a3ab579a44952d07b0c737e22cc44c
EMEA\IBA1SF <fixed-term.ahmad.ibraheem@bosch.com>
2024-12-26 09:01:00 +0200

    A parameterized query.

--- sql.js
+++ sql.js
@@ -1,26 +1,32 @@
-const { Client } = require('pg');
+const { Client } = require("pg");
 
 const client = new Client({
-  host: 'localhost',
+  host: "localhost",
   port: 5432,
-  user: 'postgres',
-  password: 'postgres',
-  database: 'taskmanager',
+  user: "postgres",
+  password: "postgres",
+  database: "taskmanager",
 });
 
-async function runQuery(query) {
+async function runQuery(query, values = []) {
   try {
     await client.connect();
-    console.log('Connected to the database');
-    const result = await client.query(query);
+    console.log("Connected to the database");
+    const result = await client.query(query, values);
     console.log(result.rows);
   } catch (err) {
-    console.error('Error:', err.stack);
+    console.error("Error:", err.stack);
   } finally {
     await client.end(); // Always close the connection
-    console.log('Client disconnected');
+    console.log("Client disconnected");
   }
 }
 
-const query = `SELECT * FROM "User"`;
-runQuery(query);
+const query = `SELECT u.name, p.title as project, t.title as task, t.completed  
+ FROM "Task" t
+ INNER JOIN "Project" p ON t."projectId" = p.id
+ INNER JOIN "User" u ON t."assigneeId" = u.id
+ WHERE t."assigneeId" = $1`;
+
+const values = [24];
+runQuery(query, values);
